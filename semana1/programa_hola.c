c771f6156483c616b6217a083d4c8d2f58241470
Hernandez Bustos Diana Ailed <hernandezbd2018@dci25-169.ugto.mx>
2018-08-08 09:41:51 -0500

    Cree un nuevo programa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,12 @@
+ /*
+     Este es un comentario, elaborado por Diana Ailed Hernández Bustos el06/08/18
+   */
+/* Creado el 25 de Enero de 2016 por Diana Hernández */
+#include<stdio.h>
+int main()
+{
+     printf("Hola Alma \n");
+     printf("ESTE es el primer programa del curso\n");
+     }
+/*Termine de escribir el programa*/
+
