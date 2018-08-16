95b84aa014f328047f40a79012d12e89aaf093d2
Hernandez Bustos Diana Ailed <hernandezbd2018@dci25-181.ugto.mx>
2018-08-16 17:06:28 -0500

    Ya hice la tarea de 4 ejercicios

---
+++ semana2/README.md
@@ -0,0 +1,31 @@
+Hecho por Diana Ailed Hernández Bustos el 16/08/18
+
+Ejercicio #1:
+En este trabajo utilicé #include<stdio.h> para incluir el contenido de la librería stdio.h que voy a utilizar para llevar a cabo el programa que se necesitan para la declaración de variables (float, int, char, etc.) y las operaciones.
+Después, utilicé la función maestra main, con el tipo int (que significa que es entero) y empecé a hacer la declaración de variables indicando el tipo de cada una (todas fueron enteras porque la edad es un número entero) y una (edad) fue de tipo char[10] ya que se trataba de letras.
+A continuación usé printf para pedir al usuario la información que necesitaba el programa por medio de un mensaje que se imprime en la pantalla, y también usé un scanf para que el usuario pudiera escribir la información que se le pedía, poniendole un %i, ya que el número que se requiere debe ser entero.
+A la hora de dar el resultado, usé un printf que tuviera %s y %i y las variables al final de nombre y edad2.
+
+Declaración de variables:
+int= número entero va a ser la variable (puede ser positivo o negativo)
+float= número decimal de hasta 6 dígitos va a ser la variable
+double= número decimal de hasta 15 dígitos va a ser la variable
+longdouble=número decimal de hasta 18 dígitos va a ser la variable
+char[10]= pide palabras de hasta 10 caracteres
+
+Ejercicio#2:
+En este programa utilicé la librería stdio.h y definí 8 variables (todas de tipo int porque deben ser enteros tanto los resultados como los números que pide al usuario). Usé 8 variables: 4 para a, b, c y d y 4 para los resultados de las 4 operaciones.
+Las operaciones no llevan tipo, simplemente se ponen como se indica en la instrucción.
+
+Ejercicio#3:
+En este programa utilicé la librería stdio.h y definí 8 variables (todas de tipo float porque tanto los resultados como los números que pide al usuario deben ser números reales). Usé 8 variables: 4 para a, b, c y d y 4 para los resultados de las 4 operaciones.
+Las operaciones no llevan tipo, simplemente se ponen como se indica en la instrucción.
+
+Diferencia ejercicio #2 y 3:
+La diferencia fue el tipo de variables que usé en cada ejercicio, en el ejercicio#2 usé int porque los números que pedía al usuario y los resultados debían ser enteros; en cambio en el ejercicio#3 usé float porque se requería que tanto los resultados fueran números reales (enteros, decimales, etc). 
+
+Ejercicio#4:
+Me di cuenta que en este ejercicio se incluía el contenido de 3 librerías diferentes. Lo que imprimía el programa era principalmente la capacidad de dígitos o los números másximos y mínimos que cada tipo de variable puede tener. No entendí lo de epsilon.
+
+
+
--- semana2/print_limites.c
+++ semana2/print_limites.c
@@ -13,45 +13,44 @@ int main( )
     printf("UINT_MAX = %u \n", UINT_MAX);
 /*Esta función muestra el número máximo ENTERO que tiene la función entero*/
     printf("ShrtINT_MAX = %i \n", SHRT_MAX);
-/*Esta función muestra el número máximo en forma entera de tipo short*/
+/*Esta función muestra el número máximo en forma entera de tipo short (es decir con máximo 5 dígitos)*/
     printf("ShrtINT_MIN = %i \n", SHRT_MIN);
-/*Esta función muestra el número mínimo en forma entera de tipo short*/
+/*Esta función muestra el número mínimo en forma entera de tipo short por eso muestra un número negativo con 5 dígitos*/
     printf("INT_MAX = %i \n", INT_MAX);
 /*Esta función muestra el número máximo de la función ENTERO en forma entera porque tiene %i*/
     printf("INT_MIN = %i \n", INT_MIN);
 /*Esta función muestra el número mínimo (es por eso que el resultado es negativo entero) en forma entera porque tiene %i*/
     printf("FLT_MIN      = %e\n", FLT_MIN);
-/*Esta función muestra el número real mínimo (puede incluir hasta 6 cifras decimales con exponente porque es float)*/
+/*Esta función muestra el número real mínimo (puede incluir hasta 6 cifras decimales con exponente negativo porque es float)*/
     printf("FLT_MAX      = %e\n", FLT_MAX);
-/*Esta función muestra el número real máximo (puede incluir hasta 6 cifras decimales con exponente porque es float) */
+/*Esta función muestra el número real máximo (puede incluir hasta 6 cifras decimales con exponente positivo porque es float) */
     printf("DBL_MIN      = %e\n", DBL_MIN);
-/*Esta función muestra el número real (puede incluir hasta el doble de cifras que float porque es double) mínimo */
+/*Esta función muestra el número real mínimo (puede incluir números más chicos que float, específicamente hasta números tan chicos con un exponente de hasta -308 porque es double) */
     printf("DBL_MAX      = %e\n", DBL_MAX);
-/*Esta función muestra el número real (puede incluir hasta el doble de cifras que float porque es double) máximo */
+/*Esta función muestra el número real máximo (puede incluir números más grandes que float, específicamente hasta números con un exponente de 308 porque es double) */
     printf("LDBL_MIN      = %Le\n",LDBL_MIN);
-/*Esta función muestra el número real (puede incluir muchos decimales, más que float o double ya que es logdouble) mínimo */
+/*Esta función muestra el número real mínimo (puede incluir números más chicos que double, específicamente hasta números con un exponente de -4932 porque es longdouble) */
     printf("LDBL_MAX      = %Le\n",LDBL_MAX);
-/*Esta función muestra el número real (puede incluir muchos decimales, más que float o double ya que es logdouble) máximo */
+/*Esta función muestra el número real máximo (puede incluir números más grandes que double, específicamente hasta números con un exponente de 4932  porque es longdouble) */
     printf("FLT_EPSILON  = %e\n", FLT_EPSILON);
-/*Esta función muestra un número real (puede incluir hasta 6 cifras porque es float)*/
+/*Esta función muestra un número real (puede incluir hasta 6 cifras decimales más exponente porque es float)*/
     printf("DBL_EPSILON  = %e\n", DBL_EPSILON);
-/*Esta función muestra un número real (puede incluir hasta 6 cifras porque es float)*/
+/*Esta función muestra un número real (puede incluir hasta 6 cifras decimales con un exponente negativo mayor que float, específicamente de -16 porque es double)*/
     printf("LBL_EPSILON  = %Le\n", LDBL_EPSILON);
-/*Esta función muestra un número real (puede incluir más cifras que float)*/
+/*Esta función muestra un número real (puede incluir hasta 6 cifras decimales con un exponente negativo mayor que double, específicamente de -19 porque es longdouble)*/
 
     printf("FLT_DIG      = %d\n", FLT_DIG);
-/*Esta función muestra un número real (puede incluir hasta 6 cifras porque es float)*/
+/*Imprime la cantidad de dígitos que permite establecer la función float, es decir 6*/
     printf("DBL_DIG      = %d\n", DBL_DIG);
-/*Esta función muestra un número real (puede incluir más de 6 cifras porque es logdouble)*/
+/*Imprime la cantidad de dígitos que permite establecer la función double, es decir 15 dígitos*/
     printf("LBDL_DIG      = %d\n", LDBL_DIG);
-/*Esta función muestra un número real (puede incluir más de 6 cifras porque es logdouble)*/
+/*Imprime la cantidad de dígitos que permite establecer la función longdouble, es decir 18 dígitos*/
     printf("FLT_MANT_DIG = %d\n", FLT_MANT_DIG);
-/*Esta función muestra un número real (puede incluir hasta 6 cifras porque es float)*/
+/*Imprime la cantidad de dígitos que permite establecer la función float con la función MANT, que es de 24 dígitos*/
     printf("DBL_MANT_DIG = %d\n", DBL_MANT_DIG);
-/*Esta función muestra un número real (puede incluir más de 6 cifras porque es double)*/
+/*Imprime la cantidad de dígitos que permite establecer la función double con la función MANT, que es de 53 dígitos*/
     printf("LDBL_MANT_DIG = %d\n", LDBL_MANT_DIG);
-/*Esta función muestra un número real (puede incluir un número mayor de cifras que float o double porque es logdouble)*/
-
+/*Imprime la cantidad de dígitos que permite establecer la función longdouble con la función MANT, que es de 64 dígitos*/
     
 }
 /*Las llaves nos indican el inicio y el final del cuerpo de un programa*/
