ed4f6a6de85fa600b144fcdf59e2bffeea048575
Hernandez Bustos Diana Ailed <diana1412hb@gmail.com>
2018-09-17 10:11:19 -0500

    Adelanté mi trabajo

--- semana7/ejercicio_integral.c
+++ semana7/ejercicio_integral.c
@@ -5,39 +5,36 @@
 
 int main(){
 
-	FILE*fp1;
-	FILE*fp2;
-	int P,i, xmin, xmax, N, k=20;
-	float h, I, res, num, suma, delta;
+	FILE *fp1;
+	FILE *fp2;
+	int P,N, k=20, i;
+	float h, I, res, suma, delta, x, xmin,xmax;
 
 	fp1=fopen("funcion.txt","r");
-	fscanf(fp1,"%i %i %i %i", &P, &xmin, &xmax, &N);
-	printf("%i %i %i %i", P, xmin, xmax, N);
+	fscanf(fp1,"%i %f %f %i", &P, &xmin, &xmax, &N);
+	printf("%i %f %f %i", P, xmin, xmax, N);
 
 	fclose(fp1);
 	fp2=fopen("resumen_funciones.txt", "w");
 
 	delta=(xmax-xmin)/N;
-	num=0;
+	h=(xmax-xmin)/k;
+	suma=0;
 
-	for(i=xmin; i<=xmax; i++){
-		num+=delta;
-		res=pow(num, P);
-	fprintf(fp2, "El número que se está evaluando es: %f\n", num);
-	fprintf(fp2, "El resultado del número elevado a la potencia es: %f\n", res);
-		
+	for(i=1; i<k; i++){
+	suma+= (xmin+i*h);
 	}
-
-	for(i=xmin; i<=xmax; i++){
-		h=(xmax-xmin)/k;
-		suma=0;
-
-		for(i=1; i<N; i++){
-		suma+= (xmin+i*h);
-		}
+	
+	for(x=xmin; x<=xmax; x+=delta){
+	res=pow(x, P);
+	fprintf(fp2,"El número que se está evaluando es: %f\n", x);
+	fprintf(fp2,"El resultado del número elevado a la potencia es: %f\n", res);
+	printf("El número que se está evaluando es: %f\n", x);
+	printf("El resultado del número elevado a la potencia es: %f\n", res);
 		
-		I=h*((xmin+i)/2)+suma;
+	I=h*((x+xmin)/2)+suma;
 	fprintf(fp2, "El resultado de la integral es: %f\n", I);
+	printf("El resultado de la integral es: %f\n", I);
 		
 	}
 	fclose(fp2);
