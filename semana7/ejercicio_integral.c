49abf58066a0c0e07cb911efc8d913568f2c0060
Hernandez Bustos Diana Ailed <diana1412hb@gmail.com>
2018-09-19 09:57:48 -0500

    Acabé los ejercicios

--- semana7/ejercicio_integral.c
+++ semana7/ejercicio_integral.c
@@ -1,46 +1,51 @@
 //Creado por Diana Ailed Hernández Bustos el 17/09/18
 
-#include<stdio.h>
-#include<math.h>
+#include<stdio.h> //Incluyo la librería que voy a usar
+#include<math.h>//Utilizo la librería math.h para usar operaciones matemáticas
 
-int main(){
+int main(){ //inicio el programa
 
-	FILE *fp1;
-	FILE *fp2;
-	int P,N, k=20, i;
-	float h, I, res, suma, delta, x, xmin,xmax;
+	FILE *fp1;//Hago la declaración de mi archivo de lectura
+	FILE *fp2;//Hago la declaración de mi archivo de escritura
+	int P, N, k, i; //Hago la declaración de mis variables enteras
+	float h, delta, x, xmin,xmax, f,res;//Hago la declaración de mis variables decimales
 
-	fp1=fopen("funcion.txt","r");
-	fscanf(fp1,"%i %f %f %i", &P, &xmin, &xmax, &N);
-	printf("%i %f %f %i", P, xmin, xmax, N);
+	fp1=fopen("funcion.txt","r");//Abro el archivo "función.txt" que es de tipo lectura
+	fscanf(fp1,"%i %f %f %i %i", &P, &xmin, &xmax, &N, &k); //Escaneo la información de mi documento de lectura
+	printf("%i %f %f %i \n", P, xmin, xmax, N);//Imprimo la información que escanée
 
-	fclose(fp1);
-	fp2=fopen("resumen_funciones.txt", "w");
+	fclose(fp1); //Cierro el documento de lectura
+	fp2=fopen("resumen_funciones.txt", "w");//Abro el archivo "resumen_funciones.txt" que es de tipo escritura
+	fprintf(fp2,"x   \t       f(x)      \t Integral \n");//Imprimo en el archivo resumen_funciones.txt 
 
-	delta=(xmax-xmin)/N;
-	h=(xmax-xmin)/k;
-	suma=0;
+	delta=(xmax-xmin)/N;//Hago la delta, es decir el cambio que hay entre cada valor de x
+		
 
-	for(i=1; i<k; i++){
-	suma+= (xmin+i*h);
+	for(x=xmin; x<=xmax; x+=delta){//Hago un FOR que recorra lo siguiente tomando los valores de x que va de mínimo al máximo con un espaciado de delta
+	h=(x-xmin)/k; //H va del valor de x menos x mínima e ntre las veces declaradas al inicio (20), ya que quiero que mi espacio se divida entre 20
+
+	res=pow(x, P);//Calculo f(x) 
+
+	f=(pow(xmin,P)+res)/2; //Divido el resultado de f(x) más f(xmínima al cuadrado)
+	
+		for(i=1; i<k; i++){ //Hago un FOR para hacer la sumatoria
+		f+= pow((xmin+i*h),P); //Hago la sumatoria 
+		}
+		
+	
+	f*=h;//Multiplico el valor de f(sumatoria) por h
+
+	fprintf(fp2,"%f \t  %f  \t  %f \n", x, res, f); //Imprimo los resultados en mi archivo de escritura
+	printf("El número que se está evaluando es: %f\n", x); //Imprimo los resultados en la pantalla para ver mis resultados
+	printf("El resultado del número elevado a la potencia es: %f\n", res); 
+	printf("El resultado de la integral es: %f\n", f);
+		
 	}
 	
-	for(x=xmin; x<=xmax; x+=delta){
-	res=pow(x, P);
-	fprintf(fp2,"El número que se está evaluando es: %f\n", x);
-	fprintf(fp2,"El resultado del número elevado a la potencia es: %f\n", res);
-	printf("El número que se está evaluando es: %f\n", x);
-	printf("El resultado del número elevado a la potencia es: %f\n", res);
-		
-	I=h*((x+xmin)/2)+suma;
-	fprintf(fp2, "El resultado de la integral es: %f\n", I);
-	printf("El resultado de la integral es: %f\n", I);
-		
-	}
-	fclose(fp2);
+	fclose(fp2);//Cierro el documento de escritura
 
 
-return 0;
+return 0; //Cierro el programa
 }
 
 
---
+++ semana7/readme.md
@@ -0,0 +1,4 @@
+Creado por Diana Ailed Hernández el 19/09/18
+
+EJERCICIO-Función
+Primero tuve que crear un archivo de texto donde tuviera la información sobre el intervalo de números, para escanear   
