9b93f34426e1108437f860320a121ec4aaed04ea
Hernandez Bustos Diana Ailed <hernandezbd2018@dci25-159.ugto.mx>
2018-10-24 09:58:45 -0500

    delanté el proyecto

--- semana11/proyecto2.c
+++ semana11/proyecto2.c
@@ -2,46 +2,42 @@
 
 #include<stdio.h>
 
-int main(int argc, char *argv[]){
+int main(){
 	FILE*fp;
-	float b1, b2, b3, b4,n, N, e=0.1;
-	float placa[x][y];
+	float bar, bab, bizq, bder, N, k, e=0.1;
+	float placa[i][j];
+	int x, y, i, j;
 
-	if(argc==5){
-		b1=atof(argv[0]);
-		b2=atof(argv[1]);
-		b3=atof(argv[2]);
-		b4=atof(argv[3]);
-		N=atoi(argv[4]);
+	fp=fopen("datos.txt", "r");
+	fscanf("%f %f %f %f %f", &bar, &bab, &bizq, &bder, &N);
+	printf("%f %f %f %f %f", bar, bab, bizq, bder, N);
+	fclose(fp);
 
-		n=sqrt(N);
+	float **A=(float**) malloc (N sizeof (float*));
 
-	for(x=0; x<N; x++){
-		placa[x][y-1]=b1;
-		placa[x][y+1]=b2
-	}
-
-	for(y=0; y<N; y++){
-		placa[x][y]=placa[x][y-1];
-		placa[x][y]=placa[x][y+1];
+	for (i=0; i<N; i++){
+		for(j=1; j<(n+1); j++){
+			placa[i][j]=0.0;
+	}	
 	}
 
 
-	for(x=1;x<N-1; x++){
-		for(y=1; y<N-1, y++){
-			placa[x][y]= (placa[x+1][y]+placa[x-1][y]+placa[x][y+1]+placa[x][y-1])/4;
-	}
-	}
-
-	if(argc>5){
-		printf("Más argumentos de los necesarios\n");
+	for(i=0; i<N; i++){
+		placa[0][j-1]=bar;
+		placa[i][j+1]=bab;
 	}
 
-	else{
-		printf("Se requiere poner las 4 temperaturas de los bordes y el número de puntos:\n");
+	for(j=0; j<N; j++ ){
+		placa[i-1][0]=bizq;
+		placa[i+1][j]=bder;
 	}
 
 
+	for(i=1; i<N-1; i++){
+		for(j=1; j<N-1; j++){
+			placa[i][j]= ((placa[i+1][j]+placa[i-1][j]+placa[i][j+1]+placa[i][j-1])/4);
+	}
+	
 return 0;
 
 }
