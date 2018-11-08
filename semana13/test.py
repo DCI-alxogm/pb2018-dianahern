b18a192212dee8c951e93fc86fdacba09ac56fbf
Hernandez Bustos Diana Ailed <diana1412hb@gmail.com>
2018-11-08 12:49:44 -0600

    Terminé ejercicio

--- semana13/test.py
+++ semana13/test.py
@@ -1,10 +1,10 @@
-import numpy as np
+import numpy as np ##Primero se hace la declaración de las librerías, una sirve para arreglos y la otra para gráficos
 import matplotlib.pyplot as plt
 
-x=np.linspace(-10,10,20)
-y1=x**2
+x=np.linspace(-10,10,20) ##Linspace te dice cómo es tu conjunto de elementos ( empieza en -10, termina en 10 y tiene 20 elementos
+y1=x**2 ##Operaciones
 y2=x**(3./2)
-plt.plot(x,y1)
+plt.plot(x,y1)##Se imrpimen los resultados en coordenadas
 plt.plot(x,y2)
-plt.show()
+plt.show() ##Sirve para mostrar el gráfico
 
